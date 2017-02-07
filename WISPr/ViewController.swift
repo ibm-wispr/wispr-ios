63f0d081cf77801815e231a32872d23df003ae70
blcheng <quasifu@brians-mbp-3.usma.ibm.com>
2017-02-07 07:36:12 -0600

    Initial Commit

---
+++ WISPr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WISPr
+//
+//  Created by Brian Cheng on 2/7/17.
+//  Copyright © 2017 Brian Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
